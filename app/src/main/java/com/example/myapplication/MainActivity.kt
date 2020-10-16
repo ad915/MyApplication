b242092eecf981ec2c1e24d59820d7aa1256edd3
ad915 <hsa901122@gmail.com>
2020-10-16 19:25:45 +0900

    두번째 커밋

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.jsoup.Jsoup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //var num:Int
+        var num= 1111111
+        Log.d("태그", "내용:" + num.toString())
+        Log.d("태그", "내용: " + num::class.simpleName) // Int
+
+        println("내용print: "+num)
+
+
+
     }
+
+
 }
